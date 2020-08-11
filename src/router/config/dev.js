19e7cda6028db2dfaf87cd5ef69a88620e0e9aad
bug <2532917800@qq.com>
2020-08-11 19:37:19 +0800

    init

---
+++ src/router/config/dev.js
@@ -0,0 +1,17 @@
+
+var apiConfig = {
+  file_api_host: 'http://testlocal.52db.xyz',
+  file_api_port: '9091',
+  file_api_prefix: '/api/file/',
+
+  user_api_host: 'http://testlocal.52db.work',
+  user_api_port: '9090',
+  user_api_prefix: '/api/user/',
+
+  captcha_api_host: 'http://testlocal.52db.work',
+  captcha_api_port: '9090',
+  captcha_api_prefix: '/api/captcha/',
+
+}
+
+module.exports = apiConfig
---
+++ src/router/config/pro.js
@@ -0,0 +1,17 @@
+
+var apiConfig = {
+  file_api_host: 'http://www.52db.xyz',
+  file_api_port: '80',
+  file_api_prefix: '/api/file/',
+
+  user_api_host: 'http://www.52db.work',
+  user_api_port: '80',
+  user_api_prefix: '/api/user/',
+
+  captcha_api_host: 'http://www.52db.work',
+  captcha_api_port: '80',
+  captcha_api_prefix: '/api/captcha/',
+
+}
+
+module.exports = apiConfig
