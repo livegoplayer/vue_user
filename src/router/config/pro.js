7721b88978753f7ab0edacdf6998456495332d6b
bug <2532917800@qq.com>
2020-08-13 21:57:19 +0800

    init

--- src/router/config/pro.js
+++ src/router/config/pro.js
@@ -5,12 +5,12 @@ var apiConfig = {
   file_api_prefix: '/api/file/',
 
   // user_api_host: 'http://www.52db.club',
-  user_api_host: 'http://139.224.132.234/',
+  user_api_host: 'http://139.224.132.234',
   user_api_port: '80',
   user_api_prefix: '/api/user/',
 
   // captcha_api_host: 'http://www.52db.club',
-  captcha_api_host: 'http://139.224.132.234/',
+  captcha_api_host: 'http://139.224.132.234',
   captcha_api_port: '80',
   captcha_api_prefix: '/api/captcha/',
 
