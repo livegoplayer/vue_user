b7fb1870716e7c48ae7c0514f2d0776075ee29d1
xjyplayer <2532917800@qq.com>
2021-05-05 16:04:53 +0800

    init

--- src/router/config/dev.js
+++ src/router/config/dev.js
@@ -1,14 +1,14 @@
 
 var apiConfig = {
-  file_api_host: 'http://file.store.k8s.show',
+  file_api_host: 'http://file-store.k8s.show',
   file_api_port: '80',
   file_api_prefix: '/api/file/',
 
-  user_api_host: 'http://go.user.k8s.show',
+  user_api_host: 'http://go-user.k8s.show',
   user_api_port: '80',
   user_api_prefix: '/api/user/',
 
-  captcha_api_host: 'http://go.user.k8s.show',
+  captcha_api_host: 'http://go-user.k8s.show',
   captcha_api_port: '80',
   captcha_api_prefix: '/api/captcha/'
 }
--- src/router/config/pro.js
+++ src/router/config/pro.js
@@ -1,16 +1,16 @@
 
 var apiConfig = {
-  file_api_host: 'http://file.store.k8s.show',
+  file_api_host: 'http://file-store.k8s.show',
   file_api_port: '80',
   file_api_prefix: '/api/file/',
 
   // user_api_host: 'http://www.52db.club',
-  user_api_host: 'http://go.user.k8s.show',
+  user_api_host: 'http://go-user.k8s.show',
   user_api_port: '80',
   user_api_prefix: '/api/user/',
 
   // captcha_api_host: 'http://www.52db.club',
-  captcha_api_host: 'http://go.user.k8s.show',
+  captcha_api_host: 'http://go-user.k8s.show',
   captcha_api_port: '80',
   captcha_api_prefix: '/api/captcha/',
 
